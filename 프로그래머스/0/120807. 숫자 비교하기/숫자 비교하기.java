fb7f56f6091616de702d896b9bcf2df4b4bd4bb2
yeonju bae <yeonju9707@naver.com>
2024-03-14 21:22:52 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.02 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int n = 0;
+        if (num1 == num2) {
+            n = 1;
+        } else {
+            n = -1;
+        };
+        return n;
+    }
+}
