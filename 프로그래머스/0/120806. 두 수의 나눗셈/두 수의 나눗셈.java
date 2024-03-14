8693e778d3b3edc9afbd52076f994e1c7ca2684b
yeonju bae <yeonju9707@naver.com>
2024-03-14 21:30:32 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.02 ms, Memory: 78 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        
+        
+        double answer =  ((double) num1 / (double) num2) * (double)1000;
+        return (int) answer;
+        
+        
+    }
+}
