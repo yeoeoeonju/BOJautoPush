f116af9fb466bab04aab0f3f583dd50ae9c67efa
yeonju bae <86589372+yeoeoeonju@users.noreply.github.com>
2024-03-26 20:42:16 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 72 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.py
@@ -1,27 +1,30 @@
 n, m = map(int, input().split())
 
-board = []
-result = []
+board = [] # 보드 만들기
+result = [] # 결과 값 중 짧은 거 고를 수 있음
 
 for i in range(n) :
-    board.append(input())
+    board.append(input()) # W, B 로 번갈아가면서 체스판을 만듦
+
 
 for i in range(n-7) :
-    for j in range(m-7):
+    for j in range(m-7): # 8x8 크기의 체스판  m-7번 판단 
+
         draw1 = 0
         draw2 = 0
+
 
         for a in range(i, i+8):
             for b in range(j, j+8):
                 if (a + b) % 2 == 0:
-                    if board[a][b] != 'B':
+                    if board[a][b] != 'B': # Black 이 아니면, white면 draw1 + 1
                         draw1 += 1 
-                    if board[a][b] != 'W' :
+                    else: # white 가 아니면, black이면 draw2 + 1  
                         draw2 += 1
                 else :
                     if board[a][b] != 'W':
                         draw1 += 1
-                    if board[a][b] != 'B':
+                    else:
                         draw2 += 1
 
         result.append(draw1)
