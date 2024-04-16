ba982e11eaa2bd88fedf27aa06a1ba01010c2863
yeonju bae <86589372+yeoeoeonju@users.noreply.github.com>
2024-04-16 12:40:08 +0900

    [level 2] Title: 숫자의 표현, Time: 2.82 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,9 +1,19 @@
 def solution(n):
-    answer = 0
+    answer = 1
     
-    for i in range(1, n+1) :
-        if n % i == 0 :
-            if i % 2 == 1 :
-                answer += 1
-                
+    for i in range(1, n//2 + 1) :
+        total = 0 
+        
+        while total < n :
+            total += i 
+            
+            if total == n :
+                answer += 1 
+                break
+            
+            i+= 1
+            
+            
+            
+            
     return answer
