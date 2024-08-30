2f15ebdf0687189ee7b1596e80139fefaebbb23f
yeonju bae <86589372+yeoeoeonju@users.noreply.github.com>
2024-08-30 12:28:43 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.37 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/README.md
+++ 프로그래머스/2/12914. 멀리 뛰기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 멀리 뛰기 - 12914 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12914) 
 
 ### 성능 요약
 
-메모리: 10.2 MB, 시간: 0.62 ms
+메모리: 10.2 MB, 시간: 0.37 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 21일 11:12:36
+2024년 08월 30일 12:28:42
 
 ### 문제 설명
 
--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -2,12 +2,17 @@ def solution(n):
     
     if n == 1 :
         return 1
+    
     else :
-        dp = [0] * (n+1)
-        dp[1] = 1
-        dp[2] = 2
+        lst = [0] * (n+1)
         
-        for i in range(3, n+1):
-            dp[i] = (dp[i-2] + dp[i-1]) % 1234567
-        return dp[-1]
+        lst[1] = 1
+        lst[2] = 2
+        
+        for i in range(3, n+1) :
+            lst[i] = (lst[i-2] + lst[i-1]) % 1234567
+            
+        return lst[-1]
+    
+    
     
