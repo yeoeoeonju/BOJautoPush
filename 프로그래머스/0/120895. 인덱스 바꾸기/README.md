6c8209fddb27da33679e625b2f6dc0c9ca735d04
yeonju bae <86589372+yeoeoeonju@users.noreply.github.com>
2024-09-13 11:07:10 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120895. 인덱스 바꾸기/README.md
+++ 프로그래머스/0/120895. 인덱스 바꾸기/README.md
@@ -1,6 +1,6 @@
 # [level 0] 인덱스 바꾸기 - 120895 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120895) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120895?language=python3) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 13일 11:03:20
+2024년 09월 13일 11:07:10
 
 ### 문제 설명
 
--- 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
+++ 프로그래머스/0/120895. 인덱스 바꾸기/인덱스 바꾸기.py
@@ -1,13 +1,9 @@
 def solution(my_string, num1, num2):
     answer = ''
     
-    for i in range(len(my_string)) :
-        if i == num1 :
-            answer += my_string[num2]
-        elif i == num2 :
-            answer += my_string[num1]
-        else :
-            answer += my_string[i]
-        
+    lst = list(my_string)
     
-    return answer
+    lst[num1], lst[num2] = lst[num2], lst[num1]
+    
+    
+    return ''.join(lst)
