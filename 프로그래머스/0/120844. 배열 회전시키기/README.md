1d1b47a5e2ea5450b0a3153b3e6b02b030f16104
yeonju bae <86589372+yeoeoeonju@users.noreply.github.com>
2024-09-21 00:07:04 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120844. 배열 회전시키기/README.md
+++ 프로그래머스/0/120844. 배열 회전시키기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 회전시키기 - 120844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120844#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120844?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.1 MB, 시간: 0.01 ms
+메모리: 10.2 MB, 시간: 0.00 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 21일 00:02:05
+2024년 09월 21일 00:07:03
 
 ### 문제 설명
 
--- 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
+++ 프로그래머스/0/120844. 배열 회전시키기/배열 회전시키기.py
@@ -1,20 +1,4 @@
 def solution(numbers, direction):
-    answer = [0] * len(numbers)
-    
-    if direction == 'right' :
-        
-        answer[0] = numbers[-1]
-        answer[-1] = numbers[-2]
-        
-        for i in range(1, len(numbers)-1):
-            answer[i] = numbers[i-1]
-        
-        return answer
-    else :
-        answer[0] = numbers[1]
-        answer[-1] = numbers[0]
-        for i in range(1, len(numbers)-1) :
-            answer[i] = numbers[i+1]
-        return answer
+    return [numbers[-1]] + numbers[:-1] if direction == 'right' else numbers[1:] + [numbers[0]]
         
         
