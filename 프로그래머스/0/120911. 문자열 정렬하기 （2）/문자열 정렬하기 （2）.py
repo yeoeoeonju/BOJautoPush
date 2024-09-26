3fe65f39af8bc0ca8e38b022809a64e366a1822e
yeonju bae <86589372+yeoeoeonju@users.noreply.github.com>
2024-09-27 07:56:20 +0900

    [level 0] Title: 문자열 정렬하기 (2), Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120911. 문자열 정렬하기 （2）/문자열 정렬하기 （2）.py
@@ -0,0 +1,11 @@
+def solution(my_string):
+    answer = []
+    my_string = my_string.lower()
+    
+    for i in my_string:
+        answer.append(i)
+    
+    answer.sort()
+    
+    
+    return ''.join(answer)
