506433245fc8590e6c69e535b14cc4b0b0a532b1
yeonju bae <86589372+yeoeoeonju@users.noreply.github.com>
2024-10-03 09:29:58 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.py
@@ -0,0 +1,9 @@
+def solution(emergency):
+    answer = []
+    temp = emergency.copy()
+    temp.sort(reverse = True)
+    
+    for i in emergency :
+        answer.append(temp.index(i)+1)
+        
+    return answer
